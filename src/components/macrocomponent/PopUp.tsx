900ea1cb1fbfe142813e7f9e549256621644ab5d
MiragemS <musantos016@gmail.com>
2024-11-24 16:43:27 -0300

    Fix: scroll adicionado

--- src/components/macrocomponent/PopUp.tsx
+++ src/components/macrocomponent/PopUp.tsx
@@ -9,7 +9,7 @@ const Popup = ({ isOpen, closePopup }) => {
         <div className=' fixed inset-0 flex w-screen h-screen bg-black z-auto opacity-30'></div>
         <div className="fixed inset-0 flex justify-end items-center z-50 top-[7rem]" onClick={closePopup}>
             <div className="animate-slide-in-right bg-[url('/imagens/bgProfile.png')] bg-cover h-screen w-[30vw]" onClick={(e) => e.stopPropagation()}> {/* Evita fechar ao clicar dentro do popup */}
-                <div className='pl-[1rem] bg-[#3b733f] h-screen opacity-95'>
+                <div className='pl-[1rem] bg-[#3b733f] h-screen opacity-95 overflow-y-auto'>
                   <div>
 
                     <div className='flex justify-between pt-[0.8rem]'>
@@ -19,7 +19,7 @@ const Popup = ({ isOpen, closePopup }) => {
 
                       {/* Acredito que possamos utilizar o componente Card com seus props para passar os dados do banco
                       e estrutura de busca / repetição para mostrar todos os produtos que estão no carrinho.*/}
-                    <div className="flex-1 overflow-y-auto px-4 space-y-4">
+                    <div className="flex-1  px-4 space-y-4">
                       <Card nome="Planta X" descricao="Aqui jaz uma bela descrição" valor="78,45" quantidade="3" />
                       <Card nome="Planta Y" descricao="Aqui jaz outra descrição" valor="50,00" quantidade="2" />
                       <Card nome="Planta Z" descricao="Aqui jaz mais uma descrição" valor="100,00" quantidade="1" />
