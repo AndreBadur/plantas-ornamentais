9ad7d83795d668087c0f65a29b88dc22f64dc4f1
MiragemS <musantos016@gmail.com>
2024-11-26 11:00:21 -0300

    Start: página de produto

---
+++ src/components/unique.tsx
@@ -0,0 +1,15 @@
+export default function Unique({}){
+    return(
+        <div>
+            <div className="flex justify-center">
+                <div className="w-[90vw] h-[75vh] mt-[4.5rem] bg-[#000000]">
+                    <div className="w-40% flex ">
+                        <div className="flex flex-col h-[100%]">
+                            <div className="w-[15vw] h-[15vh] bg-blue-500"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ src/pages/productPage.tsx
@@ -0,0 +1,11 @@
+import Menu from "@/components/Menu";
+import Unique from "@/components/unique";
+
+export default function productPage(){
+    return(
+        <div>
+            <Menu></Menu>
+            <Unique></Unique>
+        </div>
+    )
+}
