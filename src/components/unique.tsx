18ff5c74622564c9692cc99d18a55c4b4c209136
MiragemS <musantos016@gmail.com>
2024-11-26 11:12:54 -0300

    Update: desenvolvimento da estrutura

--- src/components/unique.tsx
+++ src/components/unique.tsx
@@ -4,9 +4,14 @@ export default function Unique({}){
             <div className="flex justify-center">
                 <div className="w-[90vw] h-[75vh] mt-[4.5rem] bg-[#000000]">
                     <div className="w-40% flex ">
-                        <div className="flex flex-col h-[100%]">
-                            <div className="w-[15vw] h-[15vh] bg-blue-500"></div>
+                        <div className="flex flex-col justify-center items-center h-[100%]">
+                            <div className="w-[5.2vw] h-[5.2vw] mb-[1vw] bg-blue-500"></div>
+                            <div className="w-[5.2vw] h-[5.2vw] mb-[1vw] bg-blue-500"></div>
+                            <div className="w-[5.2vw] h-[5.2vw] mb-[1vw] bg-blue-500"></div>
+                            <div className="w-[5.2vw] h-[5.2vw] mb-[1vw] bg-blue-500"></div>
+                            <div className="w-[5.2vw] h-[5.2vw] mb-[1vw] bg-blue-500"></div>
                         </div>
+                        <div className="w-[31vw] h-[30vw] mb-[1vw] ml-[1vw] bg-blue-500"></div>
                     </div>
                 </div>
             </div>
