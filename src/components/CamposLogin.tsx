1b6d6562263afeb519c9392b6d6ca78530792d0a
LuizCamargos <luiz.camargos@hotmail.com>
2024-11-26 21:56:47 -0300

    Add signup

--- src/components/CamposLogin.tsx
+++ src/components/CamposLogin.tsx
@@ -3,8 +3,9 @@ export function InputEmail() {
     <div className={`flex flex-col font-bebasNeue w-[24.125rem] `}>
       <h1>Email</h1>
       <input
-        type="text"
-        className={`border box-border border-black rounded-md h-[2.813rem] pl-[0.5rem] font-sans`}
+        type="email"
+        name="email"
+        className={`border box-border border-black rounded-md h-[2.813rem] pl-[0.5rem] font-sans  text-black`}
         placeholder="Insira seu email"
       ></input>
     </div>
@@ -17,7 +18,8 @@ export function InputPassword() {
       <h1>Senha</h1>
       <input
         type="password"
-        className={`border box-border border-black rounded-md h-[2.813rem] pl-[0.5rem] font-sans`}
+        name="password"
+        className={`border box-border border-black rounded-md h-[2.813rem] pl-[0.5rem] font-sans  text-black`}
         placeholder="Insira sua senha"
       ></input>
     </div>
@@ -26,11 +28,12 @@ export function InputPassword() {
 
 export function InputConfirmPassword() {
   return (
-    <div className={`flex flex-col font-bebasNeue w-[24.125rem] `}>
+    <div className={`flex flex-col font-bebasNeue w-[24.125rem]  text-black`}>
       <h1>Confirme sua senha</h1>
       <input
         type="password"
-        className={`border box-border border-black rounded-md h-[2.813rem] pl-[0.5rem] font-sans`}
+        name="confirm_password"
+        className={`border box-border border-black rounded-md h-[2.813rem] pl-[0.5rem] font-sans  text-black`}
         placeholder="Confirme sua senha"
       ></input>
     </div>
@@ -39,10 +42,11 @@ export function InputConfirmPassword() {
 
 export function InputName() {
   return (
-    <div className={`flex flex-col font-bebasNeue w-[24.125rem]`}>
+    <div className={`flex flex-col font-bebasNeue w-[24.125rem] text-black`}>
       <h1>Nome</h1>
       <input
         type="text"
+        name="name"
         className={`border box-border border-black rounded-md h-[2.813rem] pl-[0.5rem] font-sans`}
         placeholder="Insira seu nome"
       ></input>
--- src/pages/signUp.tsx
+++ src/pages/signUp.tsx
@@ -7,8 +7,35 @@ import {
 } from "@/components/CamposLogin";
 import { Title, SubTitle, Option, Line } from "@/components/TextoAuth";
 import Image from "next/image";
+import { useRouter } from "next/router";
+import { FormEvent } from "react";
 
 export default function SignUp() {
+  const router = useRouter()
+
+  async function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    const formData = new FormData(event.currentTarget)
+    const email = formData.get('email')
+    const password = formData.get('password')
+    const name = formData.get('name')
+    const confirm_password = formData.get('confirm_password')
+
+    const response = await fetch('/api/auth/signup', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ email, password, name, confirm_password }),
+    })
+
+    if (response.ok) {
+      router.push('/')
+    } else {
+      // Handle errors
+    }
+  }
+
+
   return (
     <div className={`flex justify-between w-[100%]`}>
       <section className={`flex justify-center items-center h-screen w-[100%]`}>
@@ -17,17 +44,24 @@ export default function SignUp() {
             <Title text="Cadastre-se agora!"></Title>
             <SubTitle text="Insira seus dados para criar uma conta!"></SubTitle>
           </div>
+          <form onSubmit={handleSubmit}>
+            <div className="h-[45vh] flex flex-col justify-around">
+              <InputName></InputName>
+              <InputEmail></InputEmail>
+              <InputPassword></InputPassword>
+              <InputConfirmPassword></InputConfirmPassword>
+            </div>
 
-          <div className="h-[45vh] flex flex-col justify-around">
-            <InputName></InputName>
-            <InputEmail></InputEmail>
-            <InputPassword></InputPassword>
-            <InputConfirmPassword></InputConfirmPassword>
-          </div>
-
-          <div className={`w-[100%] flex justify-center mt-[1.3rem]`}>
-            <Button text="Cadastrar"></Button>
-          </div>
+            <div className={`w-[100%] flex justify-center mt-[1.3rem]`}>
+              <div>
+                <button
+                  type="submit"
+                  className={`bg-[#00F511] h-[3.1rem] w-[14.8rem] rounded-lg text-[1.5rem] text-[#ffffff] font-bebasNeue`}>
+                  Cadastrar
+                </button>
+              </div>
+            </div>
+          </form>
 
           <Line></Line>
           <div className={`flex justify-center text-[0.938rem] mt-[0.4rem]`}>
